378e45c79c26d6c7f88ee9d9e54a9edc1a31e310
Lucas Vinicius <lucasprogramador77@gmail.com>
2025-11-18 15:57:46 -0300

    feat: atualizar pom e cria o modelo da classe Endereco

---
+++ README.md
@@ -0,0 +1,22 @@
+# Consulta CEP
+
+Aplicação Java que consome a API ViaCEP para buscar informações de endereços a partir de um CEP informado pelo usuário. Os dados retornados pela API são convertidos de JSON para um objeto Java utilizando a biblioteca Gson.
+
+---
+
+## 🛠 Tecnologias utilizadas
+- Java 21  
+- Biblioteca Gson  
+- API ViaCEP  
+- Maven  
+
+---
+
+## 📝 Sobre o projeto
+Este projeto foi desenvolvido com o objetivo de concluir meu curso de consumo de APIs e manipulação de arquivos, além de treinar meus conhecimentos em Java, POO e integração com serviços externos.
+
+## 🚀 Funcionalidades
+- Em andamento..  
+
+---
+
--- src/main/java/consultacep/Endereco.java
+++ src/main/java/consultacep/Endereco.java
@@ -1,5 +1,37 @@
 package cepProject.src.main.java.consultacep;
 
 public class Endereco {
-    
+        
+    private String cep;
+    private String regiao;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String local;
+    private String uf;
+    private int ddd;
+
+
+    public Endereco(String cep, String regiao, String estado, String cidade, String bairro, String local, String uf, int ddd){
+        this.cep = cep;
+        this.regiao = regiao;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.local = local;
+        this.uf = uf;
+        this.ddd = ddd;
+    }
+
+    @Override 
+    public String toString(){
+        return "\nInformações sobre o CEP: "+this.cep+"\n"+
+        "\nRegião: "+this.regiao+
+        "\nEstado: "+this.estado+
+        "\nCidade: "+this.cidade+
+        "\nBairro: "+this.bairro+
+        "\nLocal: "+this.local+
+        "\nUf: "+this.uf+
+        "\nDDD: "+this.ddd+"\n";
+    }
 }
